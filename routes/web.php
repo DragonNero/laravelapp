fe2abc674c33e34d9cf732a2852922cee3dec6cd
Dragon Nero <76917759+DragonNero@users.noreply.github.com>
2023-03-29 21:02:18 +0200

    worked on app

--- routes/web.php
+++ routes/web.php
@@ -16,25 +16,32 @@ use App\Http\Controllers\HomeController;
 */
 
 //GET
-Route::get('/blog', [PostsController::class, 'index'])->name('blog.index');
-Route::post('/blog/{id}', [PostsController::class, 'show'])->name('blog.show');
+
 
 
 //POST
-Route::get('/blog/create', [PostsController::class, 'create'])->name('blog.create');
-Route::post('/blog', [PostsController::class, 'store'])->name('blog.store');
+
 
 //PUT OR PATCH
-Route::get('/blog/edit/{id}', [PostsController::class, 'edit'])->name('blog.edit');
-Route::patch('/blog/{id}', [PostsController::class, 'update'])->name('blog.update');
 
 //DELETE
-Route::delete('/blog/{id}', [PostsController::class, 'destroy'])->name('blog.destroy');
+
+Route::prefix('/blog')->group(function() {
+    Route::get('/', [PostsController::class, 'index'])->name('blog.index');
+    Route::post('/{id}', [PostsController::class, 'show'])->name('blog.show');
+    Route::get('/create', [PostsController::class, 'create'])->name('blog.create');
+    Route::post('/', [PostsController::class, 'store'])->name('blog.store');
+    Route::get('/edit/{id}', [PostsController::class, 'edit'])->name('blog.edit');
+    Route::patch('/{id}', [PostsController::class, 'update'])->name('blog.update');
+    Route::delete('/{id}', [PostsController::class, 'destroy'])->name('blog.destroy');
+});
+
 
 // Route::resource('blog', PostsController::class);
 
 // Route for invoke methode
 Route::get('/', HomeController::class);
+
 
 
 
