49a86ff0c82371d1a04799e59ffbcaefd09c3e55
Dragon Nero <76917759+DragonNero@users.noreply.github.com>
2023-04-01 23:28:48 +0200

    added seeder

--- database/migrations/2023_03_30_212039_create_posts_table.php
+++ database/migrations/2023_03_30_212039_create_posts_table.php
@@ -13,6 +13,14 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title')->unique();
+            $table->text('excerpt')->nullable();
+            $table->text('body');
+            $table->integer('min_to_read')->default(1);
+            $table->string('image_path');
+            $table->boolean('is_published');
+
+
             $table->timestamps();
         });
     }
