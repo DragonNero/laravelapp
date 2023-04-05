ef637b998b3acf189671bef99b1174857febbd4d
Dragon Nero <76917759+DragonNero@users.noreply.github.com>
2023-04-05 22:36:08 +0200

    installed breeze

--- routes/web.php
+++ routes/web.php
@@ -1,58 +1,31 @@
 <?php
 
-use App\Http\Controllers\FallbackController;
-use Barryvdh\Debugbar\Facades\Debugbar;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PostsController;
-use App\Http\Controllers\HomeController;
 
 /*
-    GET - request a resource
-    POST - create a new resource
-    PUT - update a resource
-    PATCH - modify a resource
-    DELETE - delete a resource
-    OPTIONS - asks server which verbes are allowed
-
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
 */
 
-//GET
-
-
-
-//POST
-
-
-//PUT OR PATCH
-
-//DELETE
-
-Route::prefix('/recipe')->group(function () {
-    Route::get('/create', [PostsController::class, 'create'])->name('recipe.create');
-    Route::get('/', [PostsController::class, 'index'])->name('recipe.index');
-    Route::get('/{id}', [PostsController::class, 'show'])->name('recipe.show'); // this should be a get. Post means that you send data but when you want to show a receipe, you don,
-    Route::post('/', [PostsController::class, 'store'])->name('recipe.store');
-    Route::get('/edit/{id}', [PostsController::class, 'edit'])->name('recipe.edit');
-    Route::patch('/{id}', [PostsController::class, 'update'])->name('recipe.update');
-    Route::delete('/{id}', [PostsController::class, 'destroy'])->name('recipe.destroy');
+Route::get('/', function () {
+    return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-// Route::resource('blog', PostsController::class);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-// Route for invoke methode
-Route::get('/', HomeController::class);
-
-
-
-
-// //Multiple HTTP verbes
-// Route::match(['GET', 'POST'], '/blog', [PostsController::class, 'index']);
-// Route::any('/blog', [PostsController::class, 'index']);
-
-
-// //Return view
-
-// Route::view('/blog', 'blog.index', ['name' => 'Code with me']);
-//Fallback route
-Route::fallback(FallbackController::class);
+require __DIR__.'/auth.php';
